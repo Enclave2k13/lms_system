6fd101dc46e03f12c5be3b9795e3978d08618cd5
Nikita Komissarov <enclave13@yandex.ru>
2026-07-12 13:42:34 +0300

    31 Права доступа в DRF

--- materials/models.py
+++ materials/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
+from config import settings
+
 
 class Course(models.Model):
     """Модель курса. Содержит название, превью и описание."""
 
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='courses',
+        verbose_name='Владелец',
+    )
     name = models.CharField(max_length=200, verbose_name='Название')
     preview = models.ImageField(upload_to='course_previews/', blank=True, null=True, verbose_name='Превью')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
@@ -19,6 +29,14 @@ class Course(models.Model):
 class Lesson(models.Model):
     """Модель урока. Привязана к курсу внешним ключом."""
 
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='lessons',
+        verbose_name='Владелец',
+    )
     name = models.CharField(max_length=200, verbose_name='Название')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     preview = models.ImageField(upload_to='lesson_previews/', blank=True, null=True, verbose_name='Превью')
--- users/views.py
+++ users/views.py
@@ -1,14 +1,41 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, generics, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
 from users.models import User, Payment
-from users.serializers import UserSerializer, PaymentSerializer
+from users.serializers import (
+    UserSerializer,
+    UserPublicSerializer,
+    UserRegistrationSerializer,
+    PaymentSerializer,
+)
+from users.permissions import IsModerator
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """ViewSet для управления пользователями."""
 
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return UserPublicSerializer
+        elif self.action == 'retrieve':
+            if self.request.user.is_authenticated and self.request.user == self.get_object():
+                return UserSerializer
+            return UserPublicSerializer
+        elif self.action == 'create':
+            return UserRegistrationSerializer
+        return UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAuthenticated]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return [permission() for permission in self.permission_classes]
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
